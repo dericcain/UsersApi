1db79ba704f5599ee3c178bde6d20c0454fee07a
Deric Cain <dcain@guidewire.com>
2019-03-01 09:27:06 -0600

    Added migrations on startup in prod

--- UsersApi/Startup.cs
+++ UsersApi/Startup.cs
@@ -56,6 +56,10 @@ namespace UsersApi
             }
             else
             {
+                // Run migrations in production
+                var context = app.ApplicationServices.GetService<UsersDbContext>();
+                context.Database.Migrate();
+                
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
